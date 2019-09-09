26fa163afcb5872462b5c135a0d797a0cca7a393
pumnumber2 <pumnumber2@gmail.com>
2019-09-09 10:20:08 +0700

    Extracting React to EJS Module

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const expressLayout = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+const flash = require("connect-flash");
+const app = express();
+const session = require("express-session");
+const passport = require("passport");
+// Passport config
+require("./config/passport")(passport);
+// DB Config
+const db = require("./config/keys").MongoURI;
+// Connect to mongo
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+//EJS
+app.use(expressLayout);
+app.set("view engine", "ejs");
+
+//BodyParser
+app.use(express.urlencoded({ extended: false }));
+
+// Express Session
+app.use(
+  session({
+    secret: "ChUtIwAt_RoJaNaChAi",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect Flash
+app.use(flash());
+// Global Variable
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+});
+
+//Routes
+app.use("/", require("./routes/index"));
+app.use("/users", require("./routes/users"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MongoURI:
+    "mongodb+srv://pumnumber2:ncth0000@auth-iidfq.mongodb.net/test?retryWrites=true&w=majority"
+};
